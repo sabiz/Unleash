c9dc73384b52293ba4c02ff7c8ad092a2b602fd3
sabiz <bellett@sabiz.jp>
2020-01-05 15:38:08 +0900

    Fix udata directory

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ const CARLO_LAUNCH_OPTIONS = {
   title: package.name,
   width: appConfig.width || 1024,
   height: appConfig.height || 620,
-  userDataDir: `${__dirname}/udata`,
+  userDataDir: './udata',
 };
 
 const APP_URL = appConfig.app_url || 'https://www.google.com';
 
 launch = async () => {
   const app = await carlo.launch(CARLO_LAUNCH_OPTIONS);
-  app.serveFolder(__dirname);
+  app.serveFolder('./');
   app.on('exit', () => process.exit());
   app.on('window', () =>{
     const win = app.mainWindow();
